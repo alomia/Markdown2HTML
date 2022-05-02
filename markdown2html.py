c01c6335e499e1e8258ca79d721a04de13f4124a
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-05-02 17:20:13 -0500

    Start a script

---
+++ markdown2html.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Markdown to HTML
+
+from os import path
+from sys import argv, stderr
+
+if len(argv) <= 2:
+    print("Usage: ./markdown2html.py README.md README.html")
+    exit(1)
+elif not path.exists(argv[1]):
+    print(f"Missing {argv[1]}", file=stderr)
+    exit(1)
+else:
+    exit(0)
